2a21c2e64f7bc0b8820b393a74c340ec5ef7c978
Mariusz Nowak <mariusz@medikoo.com>
2012-11-14 19:23:36 +0100

    `serialize` function
    Currently made for relation sets, but will be used for general
    serialization as well.

---
+++ test/types/_internals/serialize.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var ObjectType = require('../../../lib/types/object');
+
+module.exports = function (t, a) {
+	var fn, x;
+	a(t(undefined), '', "Undefined");
+	a(t(null), '-', "Null");
+	a(t(false), 'b0', "Boolean");
+	a(t(-342.234), 'n-342.234', "Number");
+	a(t('misiek\nsdf\\raz\ndwa\\trzy'), 'smisiek\\nsdf\\\\raz\\ndwa\\\\trzy',
+		"String");
+	a(t(fn = function () { return 'foo'; }), 'f' + String(fn), "Function");
+	a(t(new Date(12345)), 'd12345', "Date");
+	a(t(new RegExp('raz\ndwa')), 'r/raz\\ndwa/', "RegExp");
+	a(t(ObjectType.Function), 'oFunction', "Namespace");
+	a(t(x = ObjectType({ foo: 'bar' })), 'o' + x.__id, "Object");
+	a.throws(function () { t({}); }, "Unrecognized");
+};
