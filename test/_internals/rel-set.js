bfa8c2537862acfd5d6a362e2a2bceb813219f98
Mariusz Nowak <mariusz@medikoo.com>
2012-11-22 16:30:19 +0100

    Fix count for set differences

--- test/_internals/rel-set.js
+++ test/_internals/rel-set.js
@@ -9,7 +9,8 @@ module.exports = function (t, a) {
 		foo: string.rel({ multiple: true, required: true })
 	});
 	a(typeof ns1.foo.has, 'function', "Namspace: set");
-	a.deep(ns1.foo.values, [], "Namespace: empty");
+	a.deep(ns1.foo.values, [], "Namespqace: empty");
+	a(ns1.foo.__count, 0, "Namespace: Own count");
 	a(ns1.foo.count, 0, "Namespace: Count");
 	a(ns1.foo.has('foo'), false, "Namespace: Has");
 	a(ns1.foo.has({}), false, "Namespace: Has: Invalid");
@@ -19,56 +20,57 @@ module.exports = function (t, a) {
 	});
 	a.deep(ns2.foo.values.sort(), ['raz', 'dwa', '13', 'trzy'].sort(),
 		"Extension: Content");
+	a(ns2.foo.__count, 4, "Extension: Own count");
 	a(ns2.foo.count, 4, "Extension: Count");
 	a(ns2.foo.has(13), true, "Extensions: Has: true");
 	a(ns2.foo.has('pięć'), false, "Extensions: Has: false");
 
 	a.deep(ns1.foo.values, [], "Namespace: Not affected");
-	a(ns1.foo.count, 0, "Namespace: Not affected: Count");
+	a(ns1.foo.__count, 0, "Namespace: Not affected: Own count");
 	a(ns1.foo.has(13), false, "Namespace: Not affected: Has");
 	ns1.foo = 'misko';
 	a.deep(ns1.foo.values, ['misko'], "Namespace: Reset");
-	a(ns1.foo.count, 1, "Namespace: Reset: Count");
+	a(ns1.foo.__count, 1, "Namespace: Reset: Own count");
 	a(ns1.foo.has('misko'), true, "Namespace: Reset: Has: true");
 	a(ns1.foo.has('pięć'), false, "Namespace: Reset: Has: false");
 	ns1.foo.add('marko');
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko'], "Namespace: Add");
-	a(ns1.foo.count, 2, "Namespace: Add: Count");
+	a(ns1.foo.__count, 2, "Namespace: Add: Own count");
 	a(ns1.foo.has('marko'), true, "Namespace: Add: Has: true");
 	a(ns1.foo.has('pięć'), false, "Namespace: Add: Has: false");
 
 	a.deep(ns2.foo.values.sort(), ['raz', 'dwa', '13', 'trzy'].sort(),
 		"Extension: Not Affected");
-	a(ns2.foo.count, 4, "Extension: Not affected: Count");
+	a(ns2.foo.__count, 4, "Extension: Not affected: Own count");
 	a(ns2.foo.has('dwa'), true, "Extension: Not affected: Has: true");
 	a(ns2.foo.has('marko'), false, "Extension: Not affected: Has: false");
 
 	ns2.foo = undefined;
 	a.deep(ns2.foo.values.sort(), ['marko', 'misko'].sort(),
 		"Extension: Fallback");
-	a(ns2.foo.count, 0, "Extension: Fallback: Count");
+	a(ns2.foo.__count, 0, "Extension: Fallback: Own count");
 	a(ns2.foo.has('marko'), true, "Extension: Fallback: Has: true");
 	a(ns2.foo.has('raz'), false, "Extension: Fallback: Has: false");
 
 	ns1.foo.add('next');
 	a.deep(ns2.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"Extension: Fallback: Parent add");
-	a(ns2.foo.count, 0, "Extension: Fallback: Parent add: Count");
+	a(ns2.foo.__count, 0, "Extension: Fallback: Parent add: Own count");
 	a(ns2.foo.has('next'), true, "Extension: Fallback: Parent add: Has: true");
 	a(ns2.foo.has('raz'), false, "Extension: Fallback: Parent add: Has: false");
 
 	ns2.foo.add('lorem');
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"Extension: Fallback: Add: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"Extension: Fallback: Add: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"Extension: Fallback: Add: Namespace: Not affected: Own count");
 	a(ns1.foo.has('misko'), true,
 		"Extension: Fallback: Add: Namespace: Not affected: Has: true");
 	a(ns1.foo.has('lorem'), false,
 		"Extension: Fallback: Add: Namespace: Not affected: Has: false");
 	a.deep(ns2.foo.values.sort(), ['marko', 'misko', 'next', 'lorem'].sort(),
 		"Extension: Fallback: Add: Content");
-	a(ns2.foo.count, 1, "Extension: Fallback: Add: Count");
+	a(ns2.foo.__count, 1, "Extension: Fallback: Add: Own count");
 	a(ns2.foo.has('lorem'), true,
 		"Extension: Fallback: Add: Has: true");
 	a(ns2.foo.has('trzy'), false,
@@ -77,15 +79,16 @@ module.exports = function (t, a) {
 	ns2.foo.remove('misko');
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"Extension: Fallback: Remove: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"Extension: Fallback: Remove: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"Extension: Fallback: Remove: Namespace: Not affected: Own count");
 	a(ns1.foo.has('misko'), true,
 		"Extension: Fallback: Remove: Namespace: Has: true");
 	a(ns1.foo.has('lorem'), false,
 		"Extension: Fallback: Remove: Namespace: Has: false");
 	a.deep(ns2.foo.values.sort(), ['marko', 'next', 'lorem'].sort(),
 		"Extension: Fallback: Remove: Content");
-	a(ns2.foo.count, 1, "Extension: Fallback: Remove: Count");
+	a(ns2.foo.__count, 1, "Extension: Fallback: Remove: Own count");
+	a(ns2.foo.count, 3, "Extension: Fallback: Remove: Count");
 	a(ns2.foo.has('next'), true,
 		"Extension: Fallback: Remove: Has: true");
 	a(ns2.foo.has('misko'), false,
@@ -94,7 +97,7 @@ module.exports = function (t, a) {
 	ns3 = ns2.abstract('reltest3');
 	a.deep(ns3.foo.values.sort(), ['marko', 'next', 'lorem'].sort(),
 		"2nd Extension: Fallback: Content");
-	a(ns3.foo.count, 0, "2nd Extension: Count");
+	a(ns3.foo.__count, 0, "2nd Extension: Own count");
 	a(ns3.foo.has('marko'), true,
 		"2nd Extension: Fallback: Has: true");
 	a(ns3.foo.has('misko'), false,
@@ -103,22 +106,22 @@ module.exports = function (t, a) {
 	ns3.foo.add('topl');
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"2nd Extension: Fallback: Add: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"2nd Extension: Fallback: Add: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"2nd Extension: Fallback: Add: Namespace: Not affected: Own count");
 	a(ns1.foo.has('marko'), true,
 		"2nd Extension: Fallback: Add: Namespace: Has: true");
 	a(ns1.foo.has('topl'), false,
 		"2nd Extension: Fallback: Add: Namespace: Has: false");
 	a.deep(ns2.foo.values.sort(), ['marko', 'next', 'lorem'].sort(),
 		"2nd Extension: Fallback: Add: Extension: Not affected");
-	a(ns2.foo.count, 1, "2nd Extension: Fallback: Extension: Count");
+	a(ns2.foo.__count, 1, "2nd Extension: Fallback: Extension: Own count");
 	a(ns2.foo.has('marko'), true,
 		"2nd Extension: Fallback: Add: Extension: Has: true");
 	a(ns2.foo.has('topl'), false,
 		"2nd Extension: Fallback: Add: Extension: Has: false");
 	a.deep(ns3.foo.values.sort(), ['marko', 'next', 'lorem', 'topl'].sort(),
 		"2nd Extension: Fallback: Add: Content");
-	a(ns3.foo.count, 1, "2nd Extension: Fallback: Add: Count");
+	a(ns3.foo.__count, 1, "2nd Extension: Fallback: Add: Own count");
 	a(ns3.foo.has('topl'), true,
 		"2nd Extension: Fallback: Add: Has: true");
 	a(ns3.foo.has('misko'), false,
@@ -127,22 +130,22 @@ module.exports = function (t, a) {
 	ns3.foo.remove('marko');
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"2nd Extension: Fallback: Remove: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"2nd Extension: Fallback: Remove: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"2nd Extension: Fallback: Remove: Namespace: Not affected: Own count");
 	a(ns1.foo.has('marko'), true,
 		"2nd Extension: Fallback: Remove: Namespace: Has: true");
 	a(ns1.foo.has('topl'), false,
 		"2nd Extension: Fallback: Remove: Namespace: Has: false");
 	a.deep(ns2.foo.values.sort(), ['marko', 'next', 'lorem'].sort(),
 		"2nd Extension: Fallback: Remove: Extension: Not affected");
-	a(ns2.foo.count, 1, "2nd Extension: Fallback: Extension: Count");
+	a(ns2.foo.__count, 1, "2nd Extension: Fallback: Extension: Own count");
 	a(ns2.foo.has('marko'), true,
 		"2nd Extension: Fallback: Remove: Extension: Has: true");
 	a(ns2.foo.has('topl'), false,
 		"2nd Extension: Fallback: Remove: Extension: Has: false");
 	a.deep(ns3.foo.values.sort(), ['next', 'lorem', 'topl'].sort(),
 		"2nd Extension: Fallback: Remove: Content");
-	a(ns3.foo.count, 1, "2nd Extension: Fallback: Remove: Count");
+	a(ns3.foo.__count, 1, "2nd Extension: Fallback: Remove: Own count");
 	a(ns3.foo.has('topl'), true,
 		"2nd Extension: Fallback: Remove: Has: true");
 	a(ns3.foo.has('marko'), false,
@@ -151,22 +154,22 @@ module.exports = function (t, a) {
 	ns2.foo.remove('lorem');
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"2nd Extension: Fallback: Mid remove: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"2nd Extension: Fallback: Mid remove: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"2nd Extension: Fallback: Mid remove: Namespace: Not affected: Own count");
 	a(ns1.foo.has('marko'), true,
 		"2nd Extension: Fallback: Mid remove: Namespace: Has: true");
 	a(ns1.foo.has('lorem'), false,
 		"2nd Extension: Fallback: Mid remove: Namespace: Has: false");
 	a.deep(ns2.foo.values.sort(), ['marko', 'next'].sort(),
 		"2nd Extension: Fallback: Mid remove: Extension: Content");
-	a(ns2.foo.count, 0, "2nd Extension: Fallback: Extension: Count");
+	a(ns2.foo.__count, 0, "2nd Extension: Fallback: Extension: Own count");
 	a(ns2.foo.has('marko'), true,
 		"2nd Extension: Fallback: Mid remove: Extension: Has: true");
 	a(ns2.foo.has('lorem'), false,
 		"2nd Extension: Fallback: Mid remove: Extension: Has: false");
 	a.deep(ns3.foo.values.sort(), ['next', 'topl'].sort(),
 		"2nd Extension: Fallback: Mid remove: Content");
-	a(ns3.foo.count, 1, "2nd Extension: Fallback: Mid remove: Count");
+	a(ns3.foo.__count, 1, "2nd Extension: Fallback: Mid remove: Own count");
 	a(ns3.foo.has('topl'), true,
 		"2nd Extension: Fallback: Mid remove: Has: true");
 	a(ns3.foo.has('loddrem'), false,
@@ -175,8 +178,8 @@ module.exports = function (t, a) {
 	ns2._foo.multiple = false;
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"2nd Extension: Fallback: Mid single: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"2nd Extension: Fallback: Mid single: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"2nd Extension: Fallback: Mid single: Namespace: Not affected: Own count");
 	a(ns1.foo.has('marko'), true,
 		"2nd Extension: Fallback: Mid single: Namespace: Has: true");
 	a(ns1.foo.has('lorem'), false,
@@ -188,8 +191,9 @@ module.exports = function (t, a) {
 	ns3._foo.multiple = true;
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"2nd Extension: Fallback: Top multiple: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"2nd Extension: Fallback: Top multiple: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"2nd Extension: Fallback: Top multiple: Namespace: Not affected: " +
+		"Own count");
 	a(ns1.foo.has('marko'), true,
 		"2nd Extension: Fallback: Top multiple: Namespace: Has: true");
 	a(ns1.foo.has('lorem'), false,
@@ -198,7 +202,7 @@ module.exports = function (t, a) {
 		"2nd Extension: Fallback: Top multiple: Extension: Content");
 	a.deep(ns3.foo.values.sort(), ['next', 'topl'].sort(),
 		"2nd Extension: Fallback: Top multiple: Content");
-	a(ns3.foo.count, 1, "2nd Extension: Fallback: Top multiple: Count");
+	a(ns3.foo.__count, 1, "2nd Extension: Fallback: Top multiple: Own count");
 	a(ns3.foo.has('next'), true,
 		"2nd Extension: Fallback: Top multiple: Has: true");
 	a(ns3.foo.has('lorem'), false,
@@ -207,8 +211,8 @@ module.exports = function (t, a) {
 	ns2.foo = 'radzio';
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"2nd Extension: Fallback: Set middle: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"2nd Extension: Fallback: Set middle: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"2nd Extension: Fallback: Set middle: Namespace: Not affected: Own count");
 	a(ns1.foo.has('marko'), true,
 		"2nd Extension: Fallback: Set middle: Namespace: Has: true");
 	a(ns1.foo.has('lorem'), false,
@@ -217,7 +221,7 @@ module.exports = function (t, a) {
 		"2nd Extension: Fallback: Set middle: Extension: Content");
 	a.deep(ns3.foo.values.sort(), ['radzio', 'topl'].sort(),
 		"2nd Extension: Fallback: Set middle: Content");
-	a(ns3.foo.count, 1, "2nd Extension: Fallback: Set middle: Count");
+	a(ns3.foo.__count, 1, "2nd Extension: Fallback: Set middle: Own count");
 	a(ns3.foo.has('radzio'), true,
 		"2nd Extension: Fallback: Set middle: Has: true");
 	a(ns3.foo.has('lorem'), false,
@@ -226,23 +230,24 @@ module.exports = function (t, a) {
 	ns2._foo.multiple = true;
 	a.deep(ns1.foo.values.sort(), ['marko', 'misko', 'next'].sort(),
 		"2nd Extension: Fallback: Mid multiple: Namespace: Not affected");
-	a(ns1.foo.count, 3,
-		"2nd Extension: Fallback: Mid multiple: Namespace: Not affected: Count");
+	a(ns1.foo.__count, 3,
+		"2nd Extension: Fallback: Mid multiple: Namespace: Not affected: " +
+		"Own count");
 	a(ns1.foo.has('marko'), true,
 		"2nd Extension: Fallback: Mid multiple: Namespace: Has: true");
 	a(ns1.foo.has('lorem'), false,
 		"2nd Extension: Fallback: Mid multiple: Namespace: Has: false");
 	a.deep(ns2.foo.values, ['radzio'],
 		"2nd Extension: Fallback: Mid multiple: Extension: Content");
-	a(ns2.foo.count, 1,
-		"2nd Extension: Fallback: Mid multiple: Extension: Count");
+	a(ns2.foo.__count, 1,
+		"2nd Extension: Fallback: Mid multiple: Extension: Own count");
 	a(ns2.foo.has('radzio'), true,
 		"2nd Extension: Fallback: Mid multiple: Extension: Has: true");
 	a(ns2.foo.has('lorem'), false,
 		"2nd Extension: Fallback: Mid multiple: Extension: Has: false");
 	a.deep(ns3.foo.values.sort(), ['radzio', 'topl'].sort(),
 		"2nd Extension: Fallback: Mid multiple: Content");
-	a(ns3.foo.count, 1, "2nd Extension: Fallback: Mid multiple: Count");
+	a(ns3.foo.__count, 1, "2nd Extension: Fallback: Mid multiple: Own count");
 	a(ns3.foo.has('radzio'), true,
 		"2nd Extension: Fallback: Mid multiple: Has: true");
 	a(ns3.foo.has('lorem'), false,
