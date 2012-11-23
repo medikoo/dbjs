c551fd8690f53c2bcc7e3f4ee16c451c529969d8
Mariusz Nowak <mariusz@medikoo.com>
2012-11-23 16:14:26 +0100

    Out of a box min/max validation for numbers

--- test/types/number.js
+++ test/types/number.js
@@ -3,6 +3,7 @@
 var isError     = require('es5-ext/lib/Error/is-error');
 
 module.exports = function (t, a) {
+	var ns = t.create('numtest2', { min: -100, max: 100 });
 	a.throws(function () { t(undefined); }, "Undefined");
 	a(t(null), 0, "Null");
 	a(t(false), 0, "Boolean");
@@ -11,6 +12,9 @@ module.exports = function (t, a) {
 	a(t('0'), 0, "Convertable string");
 	a(t(123), 123, "Number");
 	a(t(new Number(123)), 123, "Number object");
+	a(ns(60), 60, "Custom");
+	a.throws(function () { ns(-123); }, "Custom: Below min");
+	a.throws(function () { ns(123); }, "Custom: Above max");
 	return {
 		"Is": function (a) {
 			a(t.is(undefined), false, "Undefined");
@@ -21,6 +25,9 @@ module.exports = function (t, a) {
 			a(t.is('0'), false, "Convertable string");
 			a(t.is(123), true, "Number");
 			a(t.is(new Number(123)), false, "Number object");
+			a(ns.is(60), true, "Custom");
+			a(ns.is(-123), false, "Custom: Below min");
+			a(ns.is(123), false, "Custom: Above max");
 		},
 		"Normalize": function (a) {
 			a(t.normalize(undefined), null, "Undefined");
@@ -31,6 +38,9 @@ module.exports = function (t, a) {
 			a(t.normalize('0'), 0, "Convertable string");
 			a(t.normalize(123), 123, "Number");
 			a(t.normalize(new Number(123)), 123, "Number object");
+			a(ns.normalize(60), 60, "Custom");
+			a(ns.normalize(-123), null, "Custom: Below min");
+			a(ns.normalize(123), null, "Custom: Above max");
 		},
 		"Validate": function (a) {
 			a(isError(t.validate(undefined)), true, "Undefined");
@@ -41,6 +51,9 @@ module.exports = function (t, a) {
 			a(t.validate('0'), undefined, "Convertable string");
 			a(t.validate(123), undefined, "Number");
 			a(t.validate(new Number(123)), undefined, "Number object");
+			a(ns.validate(60), undefined, "Custom");
+			a(isError(ns.validate(-123)), true, "Custom: Below min");
+			a(isError(ns.validate(123)), true, "Custom: Above max");
 		}
 	};
 };
