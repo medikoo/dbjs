4025df83f8a505c951137f361297977ce7c66491
Mariusz Nowak <mariusz@medikoo.com>
2012-12-07 15:51:06 +0100

    Provide array serialization
    Return null on unrecognized objects

--- test/_internals/serialize.js
+++ test/_internals/serialize.js
@@ -5,8 +5,8 @@ var ObjectType = require('../../lib/types/object')
 
 module.exports = function (t, a) {
 	var fn, x;
-	a(t(undefined), undefined, "Undefined");
-	a(t(null), undefined, "Null");
+	a(t(undefined), null, "Undefined");
+	a(t(null), null, "Null");
 	a(t(false), '10', "Boolean");
 	a(t(-342.234), '2-342.234', "Number");
 	a(t('misiek\nsdf\\raz\ndwa\\trzy'), '3misiek\\nsdf\\\\raz\\ndwa\\\\trzy',
@@ -16,5 +16,6 @@ module.exports = function (t, a) {
 	a(t(new RegExp('raz\ndwa')), '5/raz\\ndwa/', "RegExp");
 	a(t(DateTime), '7DateTime', "Namespace");
 	a(t(x = new ObjectType({ foo: 'bar' })), '7' + x._id_, "Object");
-	a(t({}), undefined, "Unrecognized");
+	a(t([1,2,3]), '8"21","22","23"', "Array");
+	a(t({}), null, "Unrecognized");
 };
