8d26849e098c7f75037cf41071da1289698404de
Mariusz Nowak <mariusz@medikoo.com>
2012-12-12 18:18:35 +0100

    Deep and reverse signal events
    Now we can listen for db updates on specific objects
    plus its relations legs

--- test/_internals/signal.js
+++ test/_internals/signal.js
@@ -1,14 +1,31 @@
 'use strict';
 
-var nextTick = require('next-tick')
+var toArray  = require('es5-ext/lib/Array/from')
+  , nextTick = require('next-tick')
   , Base     = require('../../lib/types-base/base')
-  , define   = require('../../lib/_internals/define').define;
+  , define   = require('../../lib/_internals/define').define
+
+  , keys = Object.keys;
 
 module.exports = function (t, a, d) {
 	var ns = Base.create('SignalTest1');
 	setTimeout(function () {
-		var emitted = [];
-		t.on('data', function (data) { emitted.push(data); });
+		var emitted = {};
+		t.on('*', function (data) {
+			var name = '*';
+			if (!emitted[name]) emitted[name] = [];
+			emitted[name].push(arguments);
+		});
+		t.on('SignalTest1*', function (data) {
+			var name = 'SignalTest1*';
+			if (!emitted[name]) emitted[name] = [];
+			emitted[name].push(arguments);
+		});
+		t.on('SignalTest1:fooSigTest*', function (data) {
+			var name = 'SignalTest1*';
+			if (!emitted[name]) emitted[name] = [];
+			emitted[name].push(arguments);
+		});
 
 		define(ns, 'fooSigTest');
 		ns._fooSigTest.$setValue('trzy');
@@ -18,9 +35,18 @@ module.exports = function (t, a, d) {
 		t(ns._fooSigTest2, 34);
 
 		nextTick(function () {
-			var signal = emitted[0];
-			a(emitted.length, 1, "Signal emitted");
-			emitted.length = 0;
+			var signal = emitted['*'][0][2];
+			a.deep(keys(emitted).sort(), ['*', 'SignalTest1*'].sort(), "Events");
+			a(emitted['*'].length, 2, "* Events length");
+			a.deep(toArray(emitted['*'][0]), [ns._fooSigTest, 'trzy', signal, null],
+				"* Event #1");
+			a.deep(toArray(emitted['*'][1]), [ns._fooSigTest2, 34, signal, null],
+				"* Event #2");
+			a.deep(toArray(emitted['SignalTest1*'][0]),
+				[ns._fooSigTest2, 'trzy', signal], "NS* Event #2");
+
+			emitted = {};
+
 			a.deep(signal, { 'SignalTest1:fooSigTest': 'trzy',
 				'SignalTest1:fooSigTest2': 34 }, "Signal data");
 			a.deep(t.history['SignalTest1:fooSigTest'], [signal], "History #1");
@@ -33,7 +59,7 @@ module.exports = function (t, a, d) {
 
 			t.rollback();
 			nextTick(function () {
-				a(emitted.length, 0, "Rollback: Signal");
+				a(keys(emitted).length, 0, "Rollback: Signal");
 				a.deep([ns.fooSigTest, ns.fooSigTest2], ['trzy', 34],
 					"Rollback: Values");
 				a.deep(t.history['SignalTest1:fooSigTest'], [signal], "History #1");
