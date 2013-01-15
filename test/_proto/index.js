caa3c74239a0611323cc2f56cb655d83531ccbca
Mariusz Nowak <mariusz@medikoo.com>
2013-01-15 16:52:39 +0100

    Move forEachRelation to relation/instance
    Made proto/instance as main proto module

--- test/_proto/index.js
+++ test/_proto/index.js
@@ -1,26 +1,14 @@
 'use strict';
 
-var startsWith = require('es5-ext/lib/String/prototype/starts-with')
-  , Db         = require('../../')
+var contains = require('es5-ext/lib/Array/prototype/contains')
 
-  , keys = Object.keys
-  , StringType = Db.String;
+  , getPrototypeOf = Object.getPrototypeOf, keys = Object.keys;
 
-module.exports = function () {
-	return {
-		forEachRelation: function (a) {
-			var obj = Db(), data;
-			obj.set('feRelTest', StringType.rel('bar'));
-			obj.set('feRelTest2', 'foo');
-			data = [];
-			obj._forEachRelation_(function () { data.push(arguments); });
-			a(data.length, 3, "Count");
-			data.sort(function (a1, a2) {
-				return -a2[0].name.localeCompare(a1[0].name);
-			});
-			a.deep(data[0], [obj._$construct, obj._$construct._id_, obj], "Item #1");
-			a.deep(data[1], [obj._feRelTest, obj._feRelTest._id_, obj], "Item #2");
-			a.deep(data[2], [obj._feRelTest2, obj._feRelTest2._id_, obj], "Item #2");
-		}
-	};
+module.exports = function (t, a) {
+	var obj = t.$$create('$$protoTest$$');
+
+	a(getPrototypeOf(obj), t, "Prototype");
+	a.deep(keys(obj), [], "Enumerable");
+	a(obj._id_, '$$protoTest$$', "Id");
+	a(contains.call(t._children_, obj), true, "Child");
 };
