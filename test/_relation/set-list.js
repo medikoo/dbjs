2652460fc8d45ee1630c4b6fcd9c54b853bf1b2a
Mariusz Nowak <mariusz@medikoo.com>
2013-02-07 16:25:09 +0100

    Items live list for relations sets

--- test/_relation/set-list.js
+++ test/_relation/set-list.js
@@ -3,14 +3,22 @@
 var Db = require('../../');
 
 module.exports = function (a) {
-	var obj = Db({ foo: 'bar' }), list;
+	var obj = Db({ foo: 'bar' }), list, raz, bar, dwa;
 	obj._foo.multiple = true;
-	obj.foo.getItem('bar').order = 10;
-	obj.foo.add('raz').order = 5;
-	obj.foo.add('dwa').order = 20;
+	bar = obj.foo.getItem('bar');
+	bar.order = 10;
+	raz = obj.foo.add('raz');
+	raz.order = 5;
+	dwa = obj.foo.add('dwa');
+	dwa.order = 20;
 
 	list = obj.foo.list(function (a, b) {
 		return obj.foo.getItem(a).order - obj.foo.getItem(b).order;
 	});
 	a.deep(list, ['raz', 'bar', 'dwa']);
+
+	list = obj.foo.itemsList(function (a, b) {
+		return a.order - b.order;
+	});
+	a.deep(list, [raz, bar, dwa], "Items");
 };
