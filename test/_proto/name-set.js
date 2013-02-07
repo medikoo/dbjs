37e4772a79c21a5b5c910f86dd453d2eb1bf62fa
Mariusz Nowak <mariusz@medikoo.com>
2013-02-07 11:46:40 +0100

    listByOrder for name sets

--- test/_proto/name-set.js
+++ test/_proto/name-set.js
@@ -1,7 +1,9 @@
 'use strict';
 
-module.exports = function (t, a) {
-	var set = new t({}), x = {}, values, i = 0;
+var Db = require('../../');
+
+module.exports = function (t, a, d) {
+	var set = new t({}), x = {}, values, i = 0, obj, list;
 	a.deep(set.values, [], "Empty");
 	set[set._serialize('foo')] = 'foo';
 	set[set._serialize('bar')] = 'bar';
@@ -15,4 +17,26 @@ module.exports = function (t, a) {
 		a(this, x, "Context #" + i);
 		++i;
 	}, x);
+
+	obj = Db({ raz: 1, dwa: 2, trzy: 3 });
+	obj._raz.order = 10;
+	obj._dwa.order = 12;
+	obj._trzy.order = -5;
+	list = obj.getOwnPropertyNames().listByOrder();
+	a.deep(list, ['trzy', 'raz', 'dwa'], "By Order: init");
+	obj._raz.order = 20;
+	setTimeout(function () {
+		a.deep(list, ['trzy', 'dwa', 'raz'], "By Order: update");
+		obj.set('cztery', 12);
+		setTimeout(function () {
+			a.deep(list, ['trzy', 'cztery', 'dwa', 'raz'], "By Order: add");
+			obj.set('dwa');
+			setTimeout(function () {
+				a.deep(list, ['trzy', 'cztery', 'raz'], "By Order: delete");
+				obj.set('dwa');
+				a(list, obj.getOwnPropertyNames().listByOrder(), "Memoized");
+				d();
+			}, 10);
+		}, 10);
+	}, 10);
 };
