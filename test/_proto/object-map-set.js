7382fe194300e7e6a493cd0902b658c24ead5953
Mariusz Nowak <mariusz@medikoo.com>
2013-03-25 17:53:34 +0100

    Distinguish source serialize from self serialize

--- test/_proto/object-map-set.js
+++ test/_proto/object-map-set.js
@@ -26,4 +26,24 @@ module.exports = function (t, a) {
 	a(event, obj._raz, "Update: Delete: Event");
 
 	a(set, obj.getOwnPropertyNames().liveSetMap(map), "Memoized");
+
+	obj = Db({ raz: 1, trzy: 3 });
+	set = obj.getOwnPropertyNames().relMap();
+
+	a.deep(set.values.sort(byName), [obj._raz, obj._trzy], "RelMap: Mapped");
+
+	set.once('add', function (e) { event = e; });
+	obj.set('dwa', 2);
+	a.deep(set.values.sort(byName), [obj._dwa, obj._raz, obj._trzy],
+		"Update: Add");
+	a(event, obj._dwa, "RelMap: Update: Add: Event");
+
+	event = null;
+	set.once('delete', function (e) { event = e; });
+	obj.raz = undefined;
+	a.deep(set.values.sort(byName), [obj._dwa, obj._trzy],
+		"RelMap: Update: Delete");
+	a(event, obj._raz, "RelMap: Update: Delete: Event");
+
+	a(set, obj.getOwnPropertyNames().relMap(), "RelMap: Memoized");
 };
