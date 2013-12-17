6dc277232e63961577b6266ae5f6a31368e22ba4
Mariusz Nowak <mariusz@medikoo.com>
2013-12-17 10:20:30 +0100

    validDbjsObject

---
+++ test/valid-dbjs-object.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var Database = require('../');
+
+module.exports = function (t, a) {
+	var db = new Database(), obj = new db.Object(), desc, item;
+
+	a.throws(function () { t(); }, TypeError, "Undefined");
+	a.throws(function () { t(null); }, TypeError, "Null");
+	a.throws(function () { t('raz'); }, TypeError, "String");
+	a.throws(function () { t(2342); }, TypeError, "Number");
+	a.throws(function () { t(new Date()); }, TypeError, "Date");
+	a.throws(function () { t(new db.DateTime()); }, TypeError,
+		"Date out of database");
+	a.throws(function () { t({}); }, TypeError, "Plain object");
+	a(t(obj), obj, "Db object");
+	desc = obj.$get('test');
+	a(t(desc), desc, "Db descriptor");
+	desc = desc.$get('raz');
+	a(t(desc), desc, "Db descriptor's descriptor");
+	item = obj._getMultiple_('3test').$get('raz');
+	a(t(item), item, "Db item");
+};
