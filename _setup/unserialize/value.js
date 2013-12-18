c411785a22166fdfa6a57b21f3d800568760e292
Mariusz Nowak <mariusz@medikoo.com>
2013-12-18 12:22:54 +0100

    switch -> if

--- _setup/unserialize/value.js
+++ _setup/unserialize/value.js
@@ -11,29 +11,22 @@ module.exports = function (value, objects) {
 	if (!value) return undefined;
 	type = value[0];
 	value = value.slice(1);
-	switch (type) {
-	case '0':
-		return null;
-	case '1':
-		return Boolean(Number(value));
-	case '2':
-		return Number(value);
-	case '3':
-		return parse('"' + value + '"');
-	case '4':
-		return new Date(Number(value));
-	case '5':
+	if (type === '0') return null;
+	if (type === '1') return Boolean(Number(value));
+	if (type === '2') return Number(value);
+	if (type === '3') return parse('"' + value + '"');
+	if (type === '4') return new Date(Number(value));
+	if (type === '5') {
 		value = parse('"' + value + '"');
 		flags = value.slice(value.lastIndexOf('/') + 1);
 		return RegExp(value.slice(1, -(1 + flags.length)), flags);
-	case '6':
+	}
+	if (type === '6') {
 		data = parse('"' + value + '"').match(functionRe).slice(1);
 		if (data[1][0] === '\n') data[1] = data[1].slice(1);
 		if (endsWith.call(data[1], '\n')) data[1] = data[1].slice(0, -1);
 		return Function.apply(null, data);
-	case '7':
-		return objects.unserialize(value);
-	default:
-		throw new TypeError('Incorrect value');
 	}
+	if (type === '7') return objects.unserialize(value);
+	throw new TypeError('Incorrect value');
 };
