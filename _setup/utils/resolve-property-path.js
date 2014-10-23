119dac2af7425853b2e49523c8873f4661e245d2
Mariusz Nowak <medyk@medikoo.com>
2014-10-23 15:42:54 +0200

    Enrich interface of path resolver

--- _setup/utils/resolve-property-path.js
+++ _setup/utils/resolve-property-path.js
@@ -1,10 +1,27 @@
 'use strict';
 
 var value          = require('es5-ext/object/valid-value')
+  , d              = require('d')
   , unserializeId  = require('../unserialize/id')
   , unserializeKey = require('../unserialize/key')
   , object         = require('../../valid-dbjs-object')
-  , nested         = require('../../valid-dbjs-nested-object');
+  , nested         = require('../../valid-dbjs-nested-object')
+
+  , Result;
+
+Result = function (object, key) {
+	this.object = object;
+	this.key = key;
+};
+Object.defineProperties(Result.prototype, {
+	descriptor: d.gs(function () { return this.object.getDescriptor(this.key); }),
+	ownDescriptor: d.gs(function () { return this.object.getOwnDescriptor(this.key); }),
+	observable: d.gs(function () {
+		if (this.object.isKeyStatic(this.key)) return this.object[this.key];
+		return this.object.getObservable(this.key);
+	}),
+	value: d.gs(function () { return this.object.get(this.key); })
+});
 
 module.exports = function (obj, id) {
 	var names = [];
@@ -18,5 +35,5 @@ module.exports = function (obj, id) {
 		names.push(token);
 	});
 	while (names.length > 1) obj = nested(obj.get(unserializeKey(names.shift())));
-	return { object: obj, key: unserializeKey(names[0]) };
+	return new Result(obj, unserializeKey(names[0]));
 };
--- test/_setup/utils/resolve-property-path.js
+++ test/_setup/utils/resolve-property-path.js
@@ -3,7 +3,7 @@
 var Database = require('../../../');
 
 module.exports = function (t, a) {
-	var db = new Database(), obj = new db.Object();
+	var db = new Database(), obj = new db.Object(), result;
 
 	a.deep(t(obj, 'foo'), { object: obj, key: 'foo' });
 
@@ -17,5 +17,9 @@ module.exports = function (t, a) {
 		type: db.Object,
 		nested: true
 	});
-	a.deep(t(obj, 'someNested/otherNested/foo'), { object: obj.someNested.otherNested, key: 'foo' });
+	result = t(obj, 'someNested/otherNested/foo');
+	a.deep(result, { object: obj.someNested.otherNested, key: 'foo' });
+	a(result.descriptor, obj.__descriptorPrototype__);
+	a(result.ownDescriptor, obj.someNested.otherNested.getOwnDescriptor('foo'));
+	a(result.observable, obj.someNested.otherNested._foo);
 };
