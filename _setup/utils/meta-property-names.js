b87e6a25cfebdb0801f1c6f3b3f8dcf420d8f199
Mariusz Nowak <medyk@medikoo.com>
2014-10-27 15:43:33 +0100

    Add owner to meta properties

--- _setup/utils/meta-property-names.js
+++ _setup/utils/meta-property-names.js
@@ -3,5 +3,5 @@
 var primitiveSet = require('es5-ext/object/primitive-set');
 
 module.exports = primitiveSet('constructor', 'clear', 'database', 'delete',
-	'entries', 'forEach', 'has', 'key', 'keys', 'master', 'object', 'set',
+	'entries', 'forEach', 'has', 'key', 'keys', 'master', 'object', 'owner', 'set',
 	'size', 'values');
