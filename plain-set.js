43446a3760b0cd844ca9885e08c595ecdaf2417e
Mariusz Nowak <medyk@medikoo.com>
2014-10-28 17:22:41 +0100

    Add toArray to plain sets

--- plain-set.js
+++ plain-set.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var d              = require('d')
+  , aFrom          = require('es5-ext/array/from')
   , setPrototypeOf = require('es5-ext/object/set-prototype-of')
   , Set            = require('es6-set/polyfill')
 
@@ -16,5 +17,6 @@ PlainSet.prototype = Object.create(Set.prototype, {
 	last: d.gs(require('es6-set/ext/get-last')),
 	copy: d(require('es6-set/ext/copy')),
 	every: d(require('es6-set/ext/every')),
-	some: d(require('es6-set/ext/some'))
+	some: d(require('es6-set/ext/some')),
+	toArray: d(function () { return aFrom(this); })
 });
