ed71bad0c80ef1a320f587cf1c5d40a3d465bb2f
Mariusz Nowak <medyk@medikoo.com>
2014-12-11 16:25:23 +0100

    -- whitespace

--- _setup/1.property/4.descriptor-validate-value.js
+++ _setup/1.property/4.descriptor-validate-value.js
@@ -17,23 +17,12 @@ module.exports = function (descriptor) {
 		_validateDeleteValue_: d(function (obj, sKey) {
 			var nuValue;
 			if (this._reverse_) return this._reverse_._validateDelete_(obj);
-			if (this.nested) {
-				throw new DbjsError("Cannot delete nested object",
-					'NESTED_DELETE');
-			}
-			if (this.multiple) {
-				throw new DbjsError("Cannot delete multiple property",
-					'MULTIPLE_DELETE');
-			}
+			if (this.nested) throw new DbjsError("Cannot delete nested object", 'NESTED_DELETE');
+			if (this.multiple) throw new DbjsError("Cannot delete multiple property", 'MULTIPLE_DELETE');
 			if (!this.required) return sKey;
-			if (this.hasOwnProperty('_value_')) {
-				nuValue = getPrototypeOf(this)._resolveInner_(obj, sKey);
-			} else {
-				nuValue = this._resolveValueValue_(obj, sKey);
-			}
-			if (nuValue == null) {
-				throw new DbjsError("Property is required", 'VALUE_REQUIRED');
-			}
+			if (this.hasOwnProperty('_value_')) nuValue = getPrototypeOf(this)._resolveInner_(obj, sKey);
+			else nuValue = this._resolveValueValue_(obj, sKey);
+			if (nuValue == null) throw new DbjsError("Property is required", 'VALUE_REQUIRED');
 			if (isGetter(nuValue)) return sKey;
 			if (this._normalizeValue_(nuValue) == null) {
 				throw new DbjsError("Property is required", 'VALUE_REQUIRED');
@@ -46,15 +35,12 @@ module.exports = function (descriptor) {
 			}
 			if (this._reverse_) return this._reverse_._validateSet_(obj, value);
 			if (this.nested) {
-				throw new DbjsError("Nested objects cannot be overriden",
-					'NESTED_OVERRIDE');
+				throw new DbjsError("Nested objects cannot be overriden", 'NESTED_OVERRIDE');
 			}
 			if (isGetter(value)) return value;
 			if (this.multiple) return this._validateMultiple_(obj, sKey, value);
 			if (value === null) {
-				if (this.required) {
-					throw new DbjsError("Property is required", 'VALUE_REQUIRED');
-				}
+				if (this.required) throw new DbjsError("Property is required", 'VALUE_REQUIRED');
 				return value;
 			}
 			value = this.type.validate(value, this);
@@ -73,14 +59,12 @@ module.exports = function (descriptor) {
 			l = mapData.length;
 			if (!l) return value;
 			if ((l === 1) && (mapData[0] === obj)) return value;
-			throw new DbjsError(value + " is already assigned to other object",
-				'VALUE_NOT_UNIQUE');
+			throw new DbjsError(value + " is already assigned to other object", 'VALUE_NOT_UNIQUE');
 		}),
 		_validateMultiple_: d(function (obj, sKey, value) {
 			var errors, set, iKey;
 			if (value == null) {
-				throw new DbjsError("Cannot set multiple property to " + value,
-					'MULTIPLE_NULL');
+				throw new DbjsError("Cannot set multiple property to " + value, 'MULTIPLE_NULL');
 			}
 			value = toArray(value);
 			if (!value.length) {
@@ -93,16 +77,14 @@ module.exports = function (descriptor) {
 							return value; //jslint: ignore
 						}
 					}
-					throw new DbjsError("Property is required. List must not be empty",
-						'MULTIPLE_REQUIRED');
+					throw new DbjsError("Property is required. List must not be empty", 'MULTIPLE_REQUIRED');
 				}
 				return value;
 			}
 			value = uniq.call(value).map(function (value) {
 				if (value == null) {
 					if (!errors) errors = [];
-					errors.push(new DbjsError(value + " is not a value",
-						'MULTIPLE_NULL_VALUE'));
+					errors.push(new DbjsError(value + " is not a value", 'MULTIPLE_NULL_VALUE'));
 					return;
 				}
 				try {
@@ -127,8 +109,7 @@ module.exports = function (descriptor) {
 			}, this);
 			if (!errors) return value;
 			throw new DbjsError("Some values are invalid:\n\t" +
-				errors.map(getMessage).join('\t\n'), 'MULTIPLE_ERRORS',
-				{ errors: errors });
+				errors.map(getMessage).join('\t\n'), 'MULTIPLE_ERRORS', { errors: errors });
 		})
 	});
 };
