8043da7abda67c30b0c9f41f5328677ac2a4000b
Mariusz Nowak <medyk@medikoo.com>
2015-04-14 09:31:52 +0200

    Prevent injection of future time stamps

--- _setup/event.js
+++ _setup/event.js
@@ -3,10 +3,12 @@
 var d         = require('d')
   , nextTick  = require('next-tick')
   , now       = require('time-uuid/time')
+  , timeNow   = require('microtime-x')
   , serialize = require('./serialize/value')
 
   , ongoing, clear = function () { ongoing = null; }
   , increment = now.increment
+  , dayDuration = 1000 * 1000 * 60 * 60 * 24
   , count = 0, Event;
 
 Event = module.exports = function (obj, value, stamp, sourceId, index) {
@@ -23,6 +25,8 @@ Event = module.exports = function (obj, value, stamp, sourceId, index) {
 			nextTick(clear);
 			ongoing = true;
 		}
+	} else if (stamp > (timeNow() + dayDuration)) {
+		throw new TypeError("Future timestamps are not allowed");
 	}
 	this.stamp = stamp;
 	if (sourceId != null) this.sourceId = sourceId;
