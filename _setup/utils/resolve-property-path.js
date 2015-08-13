05dcf6fea7c203094ffb0a8430b6f6c50eeb8b34
Mariusz Nowak <medyk@medikoo.com>
2015-08-13 16:17:22 +0200

    Improve internals of resolvePropertyPath

--- _setup/utils/resolve-property-path.js
+++ _setup/utils/resolve-property-path.js
@@ -4,32 +4,13 @@ var value          = require('es5-ext/object/valid-value')
   , d              = require('d')
   , unserializeId  = require('../unserialize/id')
   , unserializeKey = require('../unserialize/key')
-  , serializeKey   = require('../serialize/key')
   , object         = require('../../valid-dbjs-object')
 
+  , defineProperty = Object.defineProperty
   , Result;
 
-Result = function (object, key) {
-	this.object = object;
-	this.key = key;
-};
-Object.defineProperties(Result.prototype, {
-	descriptor: d.gs(function () { return this.object.getDescriptor(this.key); }),
-	ownDescriptor: d.gs(function () { return this.object.getOwnDescriptor(this.key); }),
-	observable: d.gs(function () {
-		if (this.object.isKeyStatic(this.key)) return this.object[this.key];
-		return this.object.getObservable(this.key);
-	}),
-	value: d.gs(function () { return this.object.get(this.key); }),
-	id: d.gs(function () {
-		return this.object.__id__ + '/' + serializeKey(this.key);
-	})
-});
-
-module.exports = function (obj, id, _observe) {
-	var names = [], propValue, key;
-	object(obj);
-	names = [];
+var tokenize = function (id) {
+	var names = [];
 	unserializeId(value(id)).forEach(function (token, index) {
 		if (index % 2) {
 			if (token !== '/') throw new TypeError(id + " is not property id");
@@ -37,14 +18,51 @@ module.exports = function (obj, id, _observe) {
 		}
 		names.push(token);
 	});
+	return names;
+};
+
+var resolveObject = function (obj, names, _observe) {
+	var sKey, propValue;
 	while (names.length > 1) {
-		key = unserializeKey(names.shift());
-		propValue = obj.get(key);
+		sKey = names.shift();
+		propValue = obj._get_(sKey);
 		if (propValue == null) {
-			if (_observe) _observe(obj._get(key));
+			if (_observe) _observe(obj._getObservable_(sKey));
 			return null;
 		}
 		obj = object(propValue);
 	}
-	return new Result(obj, unserializeKey(names[0]));
+	return obj;
 };
+
+Result = function (object, sKey) {
+	this.object = object;
+	this.sKey = sKey;
+};
+Object.defineProperties(Result.prototype, {
+	descriptor: d.gs(function () { return this.object._getDescriptor_(this.sKey); }),
+	ownDescriptor: d.gs(function () { return this.object._getOwnDescriptor_(this.sKey); }),
+	observable: d.gs(function () {
+		if (this.object.isKeyStatic(this.key)) return this.object[this.key];
+		return this.object._getObservable_(this.sKey);
+	}),
+	value: d.gs(function () { return this.object._get_(this.sKey); }),
+	id: d.gs(function () {
+		return this.object.__id__ + '/' + this.sKey;
+	}),
+	key: d.gs(function () {
+		defineProperty(this, 'key', d(unserializeKey(this.sKey)));
+		return this.key;
+	})
+});
+
+module.exports = exports = function (obj, id, _observe) {
+	var names;
+	object(obj);
+	names = tokenize(id);
+	obj = resolveObject(obj, names, _observe);
+	return new Result(obj, names[0]);
+};
+exports.tokenize = tokenize;
+exports.resolveObject = resolveObject;
+exports.Result = Result;
--- test/_setup/utils/resolve-property-path.js
+++ test/_setup/utils/resolve-property-path.js
@@ -5,20 +5,20 @@ var Database = require('../../../');
 module.exports = function (t, a) {
 	var db = new Database(), obj = new db.Object(), result;
 
-	a.deep(t(obj, 'foo'), { object: obj, key: 'foo' });
+	a.deep(t(obj, 'foo'), { object: obj, sKey: 'foo' });
 
 	obj.define('someNested', {
 		type: db.Object,
 		nested: true
 	});
-	a.deep(t(obj, 'someNested/foo'), { object: obj.someNested, key: 'foo' });
+	a.deep(t(obj, 'someNested/foo'), { object: obj.someNested, sKey: 'foo' });
 
 	obj.someNested.define('otherNested', {
 		type: db.Object,
 		nested: true
 	});
 	result = t(obj, 'someNested/otherNested/foo');
-	a.deep(result, { object: obj.someNested.otherNested, key: 'foo' });
+	a.deep(result, { object: obj.someNested.otherNested, sKey: 'foo' });
 	a(result.descriptor, obj.__descriptorPrototype__);
 	a(result.ownDescriptor, obj.someNested.otherNested.getOwnDescriptor('foo'));
 	a(result.observable, obj.someNested.otherNested._foo);
