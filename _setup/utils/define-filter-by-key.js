09b41c80c111ab7a5a0d0a9515b43cef81aaf900
Mariusz Nowak <medyk@medikoo.com>
2015-08-13 17:35:46 +0200

    Add filterByKeyPath

--- _setup/utils/define-filter-by-key.js
+++ _setup/utils/define-filter-by-key.js
@@ -1,23 +1,26 @@
 'use strict';
 
-var identity          = require('es5-ext/function/identity')
-  , d                 = require('d')
-  , memoize           = require('memoizee/plain')
-  , memoizeMethods    = require('memoizee/methods-plain')
-  , getNormalizer     = require('memoizee/normalizers/get-fixed')
-  , setGetFirst       = require('es6-set/ext/get-first')
-  , setGetLast        = require('es6-set/ext/get-last')
-  , setCopy           = require('es6-set/ext/copy')
-  , setEvery          = require('es6-set/ext/every')
-  , setSome           = require('es6-set/ext/some')
-  , map               = require('observable-value/map')
-  , isObservableValue = require('observable-value/is-observable-value')
-  , DbjsError         = require('../error')
-  , serialize         = require('../serialize/key')
+var identity            = require('es5-ext/function/identity')
+  , d                   = require('d')
+  , memoize             = require('memoizee/plain')
+  , memoizeMethods      = require('memoizee/methods-plain')
+  , getNormalizer       = require('memoizee/normalizers/get-fixed')
+  , setGetFirst         = require('es6-set/ext/get-first')
+  , setGetLast          = require('es6-set/ext/get-last')
+  , setCopy             = require('es6-set/ext/copy')
+  , setEvery            = require('es6-set/ext/every')
+  , setSome             = require('es6-set/ext/some')
+  , map                 = require('observable-value/map')
+  , isObservableValue   = require('observable-value/is-observable-value')
+  , DbjsError           = require('../error')
+  , serialize           = require('../serialize/key')
+  , unserializeKey      = require('../unserialize/key')
+  , resolvePropertyPath = require('./resolve-property-path')
 
   , create = Object.create, defineProperties = Object.defineProperties
   , filterValue = function (value) { return value == null; }
   , filterNull = function (value) { return value != null; }
+  , tokenizePath = resolvePropertyPath.tokenize, resolveObject = resolvePropertyPath.resolveObject
   , resolveFilter, baseProto;
 
 resolveFilter = memoize(function (filter) {
@@ -44,10 +47,11 @@ module.exports = function (setProto) {
 						observable = map(observable, function (value) {
 							return map(filter(value, obj), Boolean);
 						});
-						result = result.value;
+						observed[obj.__id__] = result = result.value;
+					} else {
+						observed[obj.__id__] = true;
 					}
 					observable.on('change', function () { set.refresh(obj); });
-					observed[obj.__id__] = true;
 					return result;
 				}
 				if (cached !== true) return cached.value;
@@ -61,6 +65,42 @@ module.exports = function (setProto) {
 			normalizer: getNormalizer(2),
 			resolvers: [identity, resolveFilter],
 			cacheName: '__filterByKey__',
+			desc: ''
+		}),
+		filterByKeyPath: d(function (path, filter) {
+			var tokens = tokenizePath(path), sKey = tokens[tokens.length - 1], key = unserializeKey(sKey)
+			  , observed = create(null);
+			var set = this.filter(function (obj) {
+				var observable, result, cached, targetObj = resolveObject(obj, tokens);
+				if (!targetObj) return false;
+				cached = observed[obj.__id__];
+				if (!cached) {
+					if (!targetObj || (typeof targetObj._getObservable_ !== 'function')) return false;
+					if (targetObj.isKeyStatic(key)) return filter(targetObj[key], obj);
+					observable = targetObj._getObservable_(sKey);
+					result = filter(observable.value, obj);
+					if (isObservableValue(result)) {
+						observable = map(observable, function (value) {
+							return map(filter(value, obj), Boolean);
+						});
+						observed[obj.__id__] = result = result.value;
+					} else {
+						observed[obj.__id__] = true;
+					}
+					observable.on('change', function () { set.refresh(obj); });
+					return result;
+				}
+				if (cached !== true) return cached.value;
+				observable = targetObj._getObservable_(sKey);
+				result = filter(observable.value, obj);
+				if (!isObservableValue(result)) return result;
+				return result.value;
+			});
+			return set;
+		}, {
+			normalizer: getNormalizer(2),
+			resolvers: [String, resolveFilter],
+			cacheName: '__filterByKeyPath__',
 			desc: ''
 		})
 	}));
--- test/_setup/utils/define-filter-by-key.js
+++ test/_setup/utils/define-filter-by-key.js
@@ -4,32 +4,52 @@ var toArray  = require('es5-ext/array/to-array')
   , Database = require('../../../');
 
 module.exports = function (a) {
-	var db = new Database(), obj1, obj2, obj3, obj4, obj5, set, set2;
+	var db = new Database(), obj1, obj2, obj3, obj4, obj5, set, set2, set3;
 
-	obj1 = new db.Object({ foo: 'raz', bar: 'elo' });
-	obj2 = new db.Object({ foo: 'raz', bar: 'elo' });
-	obj3 = new db.Object({ foo: 'raz', bar: 'elod' });
-	obj4 = new db.Object({ foo: 'bla', bar: 'elo' });
+	db.Object.extend('ObjectExt');
+	db.ObjectExt.prototype.define('someNested', {
+		nested: true,
+		type: db.Object
+	});
 
-	set = db.Object.find('foo', 'raz').filterByKey('bar', 'elo');
-	set2 = db.Object.find('foo', 'raz').filterByKey('bar', 'miszka');
+	obj1 = new db.ObjectExt({ foo: 'raz', bar: 'elo' });
+	obj1.someNested.set('mar', 'elo');
+	obj2 = new db.ObjectExt({ foo: 'raz', bar: 'elo' });
+	obj2.someNested.set('mar', 'elo');
+	obj3 = new db.ObjectExt({ foo: 'raz', bar: 'elod' });
+	obj3.someNested.set('mar', 'elso');
+	obj4 = new db.ObjectExt({ foo: 'bla', bar: 'elo' });
+	obj4.someNested.set('mar', 'foo');
+
+	set = db.ObjectExt.find('foo', 'raz').filterByKey('bar', 'elo');
+	set2 = db.ObjectExt.find('foo', 'raz').filterByKey('bar', 'miszka');
+	set3 = db.ObjectExt.instances.filterByKeyPath('someNested/mar', 'elo');
 	a.deep(toArray(set), [obj1, obj2], "Initial");
+	a.deep(toArray(set3), [obj1, obj2]);
 
-	obj5 = new db.Object({ foo: 'raz', bar: 'elo' });
+	obj5 = new db.ObjectExt({ foo: 'raz', bar: 'elo' });
+	obj5.someNested.set('mar', 'elo');
 
 	a.deep(toArray(set), [obj1, obj2, obj5], "Create");
+	a.deep(toArray(set3), [obj1, obj2, obj5]);
 	a.deep(toArray(set2), [], "Create #2");
 
 	obj3.bar = 'elo';
 	a.deep(toArray(set), [obj1, obj2, obj5, obj3], "Fix key");
+	obj3.someNested.mar = 'elo';
+	a.deep(toArray(set3), [obj1, obj2, obj5, obj3]);
 	a.deep(toArray(set2), [], "Fix key #2");
 
 	obj4.foo = 'raz';
 	a.deep(toArray(set), [obj1, obj2, obj5, obj3, obj4], "Fix find");
+	obj4.someNested.mar = 'elo';
+	a.deep(toArray(set3), [obj1, obj2, obj5, obj3, obj4]);
 	a.deep(toArray(set2), [], "Fix find #2");
 
 	obj2.foo = 'bwa';
 	a.deep(toArray(set), [obj1, obj5, obj3, obj4], "Remove value");
+	obj2.someNested.mar = 'sdfs';
+	a.deep(toArray(set3), [obj1, obj5, obj3, obj4]);
 	a.deep(toArray(set2), [], "Remove value #2");
 
 	obj5.bar = 'miszka';
