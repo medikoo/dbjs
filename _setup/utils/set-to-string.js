df080e0c0c31d8d3c5c1d417fd12a04e44a7021b
Mariusz Nowak <medyk@medikoo.com>
2016-02-01 15:28:19 +0100

    Ensure proper stringification for multiples

--- _setup/utils/set-to-string.js
+++ _setup/utils/set-to-string.js
@@ -1,8 +1,13 @@
 'use strict';
 
 module.exports = function (/*separator*/) {
-	var data = [], sep = arguments[0];
+	var data = [], sep = arguments[0]
+	  , desc = this.object && this.object.getDescriptor(this.__pSKey__ || this.__sKey__)
+	  , db = this.object && this.object.database
+	  , toString;
 	if (sep === undefined) sep = ", ";
-	this.forEach(function (value) { data.push(String(value)); });
+	if (!desc || (desc.type === db.Base) || db.isObjectType(desc.type)) toString = String;
+	else toString = function (value) { return (new desc.type(value)).toString(desc); };
+	this.forEach(function (value) { data.push(toString(value)); });
 	return data.join(sep);
 };
