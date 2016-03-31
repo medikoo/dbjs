918fbad7db7c9c7c4d8cfc193ed374cce7cfc2cf
Mariusz Nowak <medyk@medikoo.com>
2016-03-31 15:37:16 +0200

    Relax set object serializer

--- _setup/serialize/object.js
+++ _setup/serialize/object.js
@@ -1,7 +1,11 @@
+// Object serializer used for sets so we can rely on primitive mode.
+// will become obsolete after https://github.com/medikoo/es6-set/issues/1 is resolved
+
 'use strict';
 
 module.exports = function (value) {
 	if (!value) return null;
+	if (typeof value === 'string') return value; // Allow mapping to string values
 	if (!value.__id__) return null;
 	return value.__id__;
 };
