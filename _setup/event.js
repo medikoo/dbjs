b85723e5b9a7b56e1b9716508461782e9cfd938a
Mariusz Nowak <medyk@medikoo.com>
2016-10-27 10:39:51 +0200

    Remove future timestamps prevention

--- _setup/event.js
+++ _setup/event.js
@@ -3,12 +3,10 @@
 var d         = require('d')
   , nextTick  = require('next-tick')
   , now       = require('time-uuid/time')
-  , timeNow   = require('microtime-x')
   , serialize = require('./serialize/value')
 
   , ongoing, clear = function () { ongoing = null; }
   , increment = now.increment
-  , dayDuration = 1000 * 1000 * 60 * 60 * 24
   , count = 0, Event, zeroModeCount = 0;
 
 Event = module.exports = function (obj, value, stamp, sourceId, index) {
@@ -25,8 +23,6 @@ Event = module.exports = function (obj, value, stamp, sourceId, index) {
 			nextTick(clear);
 			ongoing = true;
 		}
-	} else if (stamp > (timeNow() + dayDuration)) {
-		throw new TypeError("Future timestamps are not allowed");
 	}
 	this.stamp = stamp;
 	if (sourceId != null) this.sourceId = sourceId;
