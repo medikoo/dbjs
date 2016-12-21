a859668573f00843043435f0179d6f8828abd7ad
Mariusz Nowak <medyk@medikoo.com>
2016-12-21 12:43:48 +0100

    Add 'getDescriptor' to meta properties

--- _setup/utils/meta-property-names.js
+++ _setup/utils/meta-property-names.js
@@ -2,6 +2,5 @@
 
 var primitiveSet = require('es5-ext/object/primitive-set');
 
-module.exports = primitiveSet('constructor', 'clear', 'database', 'delete',
-	'entries', 'forEach', 'has', 'key', 'keys', 'master', 'object', 'owner', 'set',
-	'size', 'values');
+module.exports = primitiveSet('constructor', 'clear', 'database', 'delete', 'entries', 'forEach',
+	'getDescriptor', 'has', 'key', 'keys', 'master', 'object', 'owner', 'set', 'size', 'values');
